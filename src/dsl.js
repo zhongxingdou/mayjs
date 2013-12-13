312b6fc5340a23450e4834429de1317b13256f62
zhongxingdou <zhongxingdou@gmail.com>
2013-12-14 00:30:40 +0800

    Mayjs.run(fn)会在调用方法前后创建和移除wrapper dsl到Mayjs.DSL中

--- src/dsl.js
+++ src/dsl.js
@@ -1,32 +1,45 @@
 Mayjs.util.run(function(M) {
-    M._ = M.$();
     M.run = function(fn){
+        var wrapper = M.$wrapper();
+        M.MObjectUtil.mix(M.DSL, wrapper);
+
         var args = [fn, M].concat(Array.prototype.slice.call(arguments, 1));
-        return M.util.run.apply(this, args);
+        try{
+            var result = M.util.run.apply(this, args);
+        }catch(e){
+            throw e;
+        }finally{
+            Object.keys(wrapper).forEach(function(k){
+                delete M.DSL[k];
+            });
+        }
+        return result;
+    }
+
+    M.DSL = {
+        $checkArgu: M.$checkArgu,
+        $class: M.$class,
+        $clone: M.MObjectUtil.clone,
+        $func: M.$func,
+        $enum: M.util.enumeration,
+        $fn: M.util.fn,
+        $include: M.$include,
+        $implement: M.$implement,
+        $interface: M.$interface,
+        $is: M.$is,
+        $merge: M.MObjectUtil.merge,
+        $mix: M.MObjectUtil.mix,
+        $module: M.$module,
+        $obj: M.$obj,
+        $run: M.util.run,
+        $support: M.$support,
+        $overload: M.$overload,
+        $overwrite: M.util.overwrite,
+        $methodize: M.util.methodize,
+        $wrapper: M.$wrapper
     }
 
     M.importDSL = function() {
-        return M.util.dsl({
-            $checkArgu: M.$checkArgu,
-            $class: M.$class,
-            $clone: M.MObjectUtil.clone,
-            $func: M.$func,
-            $enum: M.util.enumeration,
-            $fn: M.util.fn,
-            $include: M.$include,
-            $implement: M.$implement,
-            $interface: M.$interface,
-            $is: M.$is,
-            $merge: M.MObjectUtil.merge,
-            $mix: M.MObjectUtil.mix,
-            $module: M.$module,
-            $obj: M.$obj,
-            $run: M.util.run,
-            $support: M.$support,
-            $overload: M.$overload,
-            $overwrite: M.util.overwrite,
-            $methodize: M.util.methodize,
-            _: M._
-        });
+        return M.util.dsl(M.DSL);
     }
 }, Mayjs);
